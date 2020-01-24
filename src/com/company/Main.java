6c60de254987d679caba00997c127dfb0625e431
nedzlam <nedzlamemic@icloud.com>
2020-01-24 21:06:32 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> lista = new ArrayList<>();
+        int suma = 0;
+        int pok = 0;
+        for(;;) {
+            lista.add(scanner.nextLine());
+            if(lista.contains("")) break;
+        }
+        for(int i=0; i<lista.size(); i++) {
+            System.out.printf(lista.get(i) + " ");
+            try {
+            suma = suma + Integer.parseInt(lista.get(i));
+        }catch (NumberFormatException e){
+                //exception;
+            }
+    }
+        System.out.println("Result is: " +suma);
+}}
